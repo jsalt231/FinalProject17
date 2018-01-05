2b0c903a91621b721f5954c371b32495d438a7a7
Jason Salmanowitz <jsalmanowitz@ucvts.org>
2018-01-05 16:25:31 +0000

    added description in README.md, fixed assignment bugs and added inventories

---
+++ README.md
@@ -0,0 +1,10 @@
+Welcome to Revolution Simulator 1918!
+
+-Pick between 3 character: Leo, Joey, and Vlad
+-Character goes through campaign story mode by picking numbered options to do tasks
+-Final boss fight: Tsar Nicholas II
+-Endings depend on character (or maybe inventory?)
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!! SPOILERS BELOW !!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!
--- characters_and_enemies.py
+++ characters_and_enemies.py
@@ -3,22 +3,33 @@ joey_stats = {'str': 7, 'int': 1, 'agi': 5, 'cha': 3, 'health': 120}
 vlad_stats = {'str': 2, 'int': 7, 'agi': 1, 'cha': 6, 'health': 80}
 
 def assignment():
+    while True:
         answer = input("Who would you like to play? Enter 1 for Leo, 2 for Joey, and 3 for Vlad\n")
         answer = int(answer)
         if answer == 1:
             player = 'leo'
+            break
         elif answer == 2:
             player = 'joey'
+            break
         elif answer == 3:
             player = 'vlad'
+            break
         else:
             print("I'm sorry, that's not an option. Try again!")
-        print(f"You have chosen to play as {player.title()}")
-        if player == 'leo':
-            player_stats = leo_stats
-        if player == 'joey':
-            player_stats = joey_stats
-        if player == 'vlad':
-            player_stats = vlad_stats
-        print(f"""Your stats as as follows:
-        Strength = {player_stats['str']}, Intelligence = {player_stats['int']}, Agility = {player_stats['agi']}, and Charisma = {player_stats['cha']}""")
+
+    print(f"You have chosen to play as {player.title()}")
+    if player == 'leo':
+        player_stats = leo_stats
+        player_inventory = ['flag']
+    if player == 'joey':
+        player_stats = joey_stats
+        player_inventory = ['rifle']
+    if player == 'vlad':
+        player_stats = vlad_stats
+        player_inventory = ['book']
+    print(f"""Your stats as as follows:
+    Strength = {player_stats['str']}, Intelligence = {player_stats['int']}, Agility = {player_stats['agi']}, and Charisma = {player_stats['cha']}
+You have the following in your inventory:
+    {player_inventory[0].title()}""")
+
